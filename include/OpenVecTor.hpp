b6bfa8805777632b701c32310977e1e167e78d38
roseleblood <info@roseleblood.nrw>
2025-02-13 00:56:15 +0100

    add version and fast-_type

--- include/OpenVecTor.hpp
+++ include/OpenVecTor.hpp
@@ -1,135 +1,42 @@
 #ifndef __vector_base_h_
 #define __vector_base_h_ 1
 
-#include <KHR/khrplatform.h>
+#include "VT/OVplatform.h"
+#include "VT/OVfasttype.hpp"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#if defined(_WIN32) && !defined(APIENTRY) && !defined(__CYGWIN__) && !defined(__SCITECH_SNAP__)
-#define WIN32_LEAN_AND_MEAN 1
-#include <windows.h>
-#endif
-
-#ifndef APIENTRY
-#define APIENTRY
-#endif
-#ifndef APIENTRYP
-#define APIENTRYP APIENTRY *
-#endif
-#ifndef VECTORAPI
-#define VECTORAPI extern
-#endif
-
-#if defined(OPENVECTOR_STATIC)
-#   define OPENVECTOR_PUBLICAPI
-#elif defined(_WIN32)
-#   define OPENVECTOR_PUBLICAPI __declspec(dllimport)
-#elif defined (__SYMBIAN32__)
-#   define OPENVECTOR_PUBLICAPI IMPORT_C
-#elif defined(__ANDROID__)
-#   define OPENVECTOR_PUBLICAPI __attribute__((visibility("default")))
-#else
-#   define OPENVECTOR_PUBLICAPI
-#endif
-
-#if defined(_WIN32) && !defined(_WIN32_WCE) && !defined(__SCITECH_SNAP__)
-    /* Win32 but not WinCE */
-#   define OPENVECTOR_APIENTRY __stdcall
-#else
-#   define OPENVECTOR_APIENTRY
-#endif
-
-#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__GNUC__) || defined(__SCO__) || defined(__USLC__)
-// Linux
-#include <stdint.h>
-
-typedef int32_t                 ov_sint_t;
-typedef uint32_t                ov_uint_t;
-typedef int64_t                 ov_slong_t;
-typedef uint64_t                ov_ulong_t;
-typedef          float          ov_float_t;
-
-#define OPENVECTOR_SUPPORT_INT64   1
-#define OPENVECTOR_SUPPORT_FLOAT   1
-
-#elif defined(__sun__) || defined(__digital__)
-/*
- * Sun or Digital
- */
-typedef int                     ov_sint_t;
-typedef unsigned int            ov_uint_t;
-typedef          float          ov_float_t;
-
-#if defined(__arch64__) || defined(_LP64)
-typedef long int                ov_slong_t;
-typedef unsigned long int       ov_ulong_t;
-#else
-typedef long long int           ov_slong_t;
-typedef unsigned long long int  ov_ulong_t;
-#endif /* __arch64__ */
-
-#define OPENVECTOR_SUPPORT_INT64   1
-#define OPENVECTOR_SUPPORT_FLOAT   1
-
-
-#elif defined(_WIN32) && !defined(__SCITECH_SNAP__)
-/*
- * Win32
- */
-typedef __int32                 ov_sint_t;
-typedef unsigned __int32        ov_uint_t;
-typedef __int64                 ov_slong_t;
-typedef unsigned __int64        ov_ulong_t;
-typedef          float          ov_float_t;
-#define OPENVECTOR_SUPPORT_INT64   1
-#define OPENVECTOR_SUPPORT_FLOAT   1
-
-#else
-#include <stdint.h>
-
-typedef int32_t                 ov_sint_t;
-typedef uint32_t                ov_uint_t;
-
-#define OPENVECTOR_SUPPORT_INT64   0
-#define OPENVECTOR_SUPPORT_FLOAT   0
-#endif
-
-typedef signed   char          ov_schar_t;
-typedef unsigned char          ov_uchar_t;
-typedef signed   short int     ov_sshort_t;
-typedef unsigned short int     ov_ushort_t;
-typedef intptr_t               ov_intptr_t;
-typedef uintptr_t              ov_uintptr_t;
-
-typedef enum {
-    OV_FALSE = 0,
-    OV_TRUE  = 1,
-} ov_boolean_t;
-
-#if __cplusplus >= 201703L
-# define CL_HPP_DEFINE_STATIC_MEMBER_ inline
-#elif defined(_MSC_VER)
-# define CL_HPP_DEFINE_STATIC_MEMBER_ __declspec(selectany)
-#elif defined(__MINGW32__)
-# define CL_HPP_DEFINE_STATIC_MEMBER_ __attribute__((selectany))
-#else
-# define CL_HPP_DEFINE_STATIC_MEMBER_ __attribute__((weak))
-#endif // !_MSC_VER
-
+#define VECTOR_COPYRIGHT "Copyright (c) 2025 Amber-Sophia Schroeck " \
+                       "MIT License"
 
 #define VECTOR_VERSION 010000
 #define VECTOR_VERSION_MAJOR    1
 #define VECTOR_VERSION_MINOR    0
 #define VECTOR_VERSION_PATCH    0
 
+#define VECTOR_VERSION_AT_LEAST(major, minor, patch ) 	\
+(((major) < VECTOR_VERSION_MAJOR)                                     \
+ || ((major) == VECTOR_VERSION_MAJOR && (minor) < VECTOR_VERSION_MINOR) \
+ || ((major) == VECTOR_VERSION_MAJOR && (minor) == VECTOR_VERSION_MINOR && (patch) <= VECTOR_VERSION_PATCH))	
+
+#define VECTOR_STRINGIFY(n) VECTOR_STRINGIFY_HELPER(n)
+#define VECTOR_STRINGIFY_HELPER(n) #n
+
+#define VECTOR_VERSION_STRING VECTOR_STRINGIFY_HELPER(VECTOR_VERSION_MAJOR) "." \
+     VECTOR_STRINGIFY_HELPER(VECTOR_VERSION_MINOR) "." VECTOR_STRINGIFY_HELPER(VECTOR_VERSION_PATCH) 
+     
 #ifndef VECTOR_VERSION_1_0
 #define VECTOR_VERSION_1_0 1
 
+typedef struct {
+	ov_schar_t major;      /**< major number */
+	ov_schar_t minor;      /**< minor number */
+	ov_schar_t patch;      /**< patch number */
+} ov_version_t;
+  
 typedef const char* (APIENTRYP PFNOPENVECTORVERSIONPROC) (void);
+typedef const char* (APIENTRYP PFNOPENVECTORVERSIONSTRINGPROC) (void);
 
 VECTORAPI const char* APIENTRY openvector_version (void);
+VECTORAPI const char* APIENTRY openvector_version_string(void);
 #endif
 
 
---
+++ include/VT/OVplatform.h
@@ -0,0 +1,134 @@
+#ifndef __vector_platform_h_
+#define __vector_platform_h_ 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) && !defined(APIENTRY) && !defined(__CYGWIN__) && !defined(__SCITECH_SNAP__)
+#define WIN32_LEAN_AND_MEAN 1
+#include <windows.h>
+#endif
+
+#ifndef APIENTRY
+#define APIENTRY
+#endif
+#ifndef APIENTRYP
+#define APIENTRYP APIENTRY *
+#endif
+#ifndef VECTORAPI
+#define VECTORAPI extern
+#endif
+
+#if defined(OPENVECTOR_STATIC)
+#   define OPENVECTOR_PUBLICAPI
+#elif defined(_WIN32)
+#   define OPENVECTOR_PUBLICAPI __declspec(dllimport)
+#elif defined (__SYMBIAN32__)
+#   define OPENVECTOR_PUBLICAPI IMPORT_C
+#elif defined(__ANDROID__)
+#   define OPENVECTOR_PUBLICAPI __attribute__((visibility("default")))
+#else
+#   define OPENVECTOR_PUBLICAPI
+#endif
+
+#if defined(_WIN32) && !defined(_WIN32_WCE) && !defined(__SCITECH_SNAP__)
+    /* Win32 but not WinCE */
+#   define OPENVECTOR_APIENTRY __stdcall
+#else
+#   define OPENVECTOR_APIENTRY
+#endif
+
+#if __cplusplus >= 201703L
+# define CL_HPP_DEFINE_STATIC_MEMBER_ inline
+#elif defined(_MSC_VER)
+# define CL_HPP_DEFINE_STATIC_MEMBER_ __declspec(selectany)
+#elif defined(__MINGW32__)
+# define CL_HPP_DEFINE_STATIC_MEMBER_ __attribute__((selectany))
+#else
+# define CL_HPP_DEFINE_STATIC_MEMBER_ __attribute__((weak))
+#endif // !_MSC_VER
+
+#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__GNUC__) || defined(__SCO__) || defined(__USLC__)
+// Linux
+#include <stdint.h>
+
+typedef int32_t                 ov_sint_t;
+typedef uint32_t                ov_uint_t;
+typedef int64_t                 ov_slong_t;
+typedef uint64_t                ov_ulong_t;
+typedef float                   ov_float_t;
+typedef double                  ov_double_t;
+
+#define OPENVECTOR_SUPPORT_INT64   1
+#define OPENVECTOR_SUPPORT_FLOAT   1
+#define OPENVECTOR_SUPPORT_DOUBLE  1
+
+#elif defined(__sun__) || defined(__digital__)
+/*
+ * Sun or Digital
+ */
+typedef int                     ov_sint_t;
+typedef unsigned int            ov_uint_t;
+typedef          float          ov_float_t;
+
+#if defined(__arch64__) || defined(_LP64)
+typedef long int                ov_slong_t;
+typedef unsigned long int       ov_ulong_t;
+#else
+typedef long long int           ov_slong_t;
+typedef unsigned long long int  ov_ulong_t;
+#endif /* __arch64__ */
+
+#define OPENVECTOR_SUPPORT_INT64   1
+#define OPENVECTOR_SUPPORT_FLOAT   1
+#define OPENVECTOR_SUPPORT_DOUBLE  0
+
+
+#elif defined(_WIN32) && !defined(__SCITECH_SNAP__)
+/*
+ * Win32
+ */
+typedef __int32                 ov_sint_t;
+typedef unsigned __int32        ov_uint_t;
+typedef __int64                 ov_slong_t;
+typedef unsigned __int64        ov_ulong_t;
+typedef float          		ov_float_t;
+typedef double                  ov_double_t;
+
+#define OPENVECTOR_SUPPORT_INT64   1
+#define OPENVECTOR_SUPPORT_FLOAT   1
+#define OPENVECTOR_SUPPORT_DOUBLE  1
+
+#else
+#include <stdint.h>
+
+typedef int32_t                 ov_sint_t;
+typedef uint32_t                ov_uint_t;
+
+#define OPENVECTOR_SUPPORT_INT64   0
+#define OPENVECTOR_SUPPORT_FLOAT   0
+#define OPENVECTOR_SUPPORT_DOUBLE  0
+
+#endif
+
+typedef signed   char          ov_schar_t;
+typedef unsigned char          ov_uchar_t;
+typedef signed   short int     ov_sshort_t;
+typedef unsigned short int     ov_ushort_t;
+typedef intptr_t               ov_intptr_t;
+typedef uintptr_t              ov_uintptr_t;
+
+typedef enum {
+    OV_FALSE = 0,
+    OV_TRUE  = 1,
+} ov_boolean_t;
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __vector_platform_h_
